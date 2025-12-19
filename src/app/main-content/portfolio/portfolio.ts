7d63052688450301bca3ffc10a1930e7becb17e8
Henning Otte <deine-email@beispiel.de>
2025-12-19 09:59:53 +0100

    Add portfolio component

---
+++ src/app/main-content/portfolio/portfolio.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProjectCard } from './project-card/project-card';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [ProjectCard],
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.scss',
+})
+export class Portfolio {
+
+}
