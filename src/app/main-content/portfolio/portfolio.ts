b15f5b7f21df8182d67aa42aafe58ceab62afefe
Henning Otte <deine-email@beispiel.de>
2025-12-19 20:51:47 +0100

    feat: enhance portfolio and project card components with dynamic data binding and styling

--- src/app/main-content/portfolio/portfolio.ts
+++ src/app/main-content/portfolio/portfolio.ts
@@ -9,34 +9,38 @@ import { CommonModule } from '@angular/common';
   styleUrl: './portfolio.scss',
 })
 export class Portfolio {
-  projects: { name: string; techstack: string[]; description: string, img: string }[] = [
+  projects: { name: string; techstack: string[]; description: string; img: string }[] = [
     {
       name: 'Join',
       techstack: ['JavaScript', 'CSS', 'HTML', 'Firebase'],
       description:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        img: 'join_laptop_mockup',
+      img: 'join_laptop_mockup.png',
     },
     {
       name: '1-Bit Horror Game',
       techstack: ['JavaScript', 'CSS', 'HTML'],
       description:
-        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        img: 'join_laptop_mockup',
+        'A jump, run, and throw game built using object-oriented principles. Collect coins and rare fireballs scattered across the map to defeat skeletons and a dangerous cyclops—who can only be defeated using these scarce fireballs.',
+      img: '1bit_horror_game_laptop_mockup.png',
     },
     {
-      name: 'Join',
-      techstack: ['JavaScript', 'CSS', 'HTML', 'Firebase'],
+      name: 'Pokedex',
+      techstack: ['JavaScript', 'CSS', 'HTML'],
       description:
-        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        img: 'join_laptop_mockup',
+        'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      img: 'pokemon_api_laptop_mockup.png',
     },
     {
-      name: 'Join',
+      name: 'Ring of Fire',
       techstack: ['JavaScript', 'CSS', 'HTML', 'Firebase'],
       description:
-        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        img: 'join_laptop_mockup',
+        'An Angular-based multiplayer game using Firebase for real-time data synchronization and player state management.',
+      img: 'ring-of-fire_laptop_mockup.png',
     },
   ];
+
+  flexDirection(i: boolean) {
+    return i == true ? 'row-reverse' : 'row';
+  }
 }
