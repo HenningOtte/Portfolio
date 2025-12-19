2b262ca28f6cb3d82047d05ffb7855f07694230b
Henning Otte <HenningOtte91@gmail.com>
2025-12-19 09:59:53 +0100

    Add portfolio component

--- src/app/main-content/main-content.ts
+++ src/app/main-content/main-content.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Hero } from './hero/hero';
 import { AboutMe } from './about-me/about-me';
 import { MySkills } from './my-skills/my-skills';
+import { Portfolio } from './portfolio/portfolio';
 
 @Component({
   selector: 'app-main-content',
-  imports: [Hero, AboutMe, MySkills, CommonModule],
+  imports: [Hero, AboutMe, MySkills, CommonModule, Portfolio],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
