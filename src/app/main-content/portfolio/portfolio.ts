284ed3c4e1cf5a62c30a97bc929e03b113293665
Henning Otte <HenningOtte91@gmail.com>
2025-12-19 16:26:27 +0100

    Add styling and logic to portfolio component

--- src/app/main-content/portfolio/portfolio.ts
+++ src/app/main-content/portfolio/portfolio.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 import { ProjectCard } from './project-card/project-card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [ProjectCard],
+  imports: [ProjectCard, CommonModule],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.scss',
 })
 export class Portfolio {
-
+  projects: { name: string; techstack: string[]; description: string, img: string }[] = [
+    {
+      name: 'Join',
+      techstack: ['JavaScript', 'CSS', 'HTML', 'Firebase'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+        img: 'join_laptop_mockup',
+    },
+    {
+      name: '1-Bit Horror Game',
+      techstack: ['JavaScript', 'CSS', 'HTML'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+        img: 'join_laptop_mockup',
+    },
+    {
+      name: 'Join',
+      techstack: ['JavaScript', 'CSS', 'HTML', 'Firebase'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+        img: 'join_laptop_mockup',
+    },
+    {
+      name: 'Join',
+      techstack: ['JavaScript', 'CSS', 'HTML', 'Firebase'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+        img: 'join_laptop_mockup',
+    },
+  ];
 }
