f5a58591ccb43cfcdd2f90147702de1494e676b8
Henning Otte <deine-email@beispiel.de>
2025-12-23 21:31:41 +0100

    Add logic to the contact component and extend button styles (SCSS).

--- src/app/main-content/contact/contact.ts
+++ src/app/main-content/contact/contact.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
+  privacyPolicy: boolean = false;
+  name: string = '';
+  email: string = '';
+  message: string = '';
 
+  constructor() {}
+
+  privacyChecked() {
+    const checkbox: any = document.querySelector('.privacy-container')?.children[0];
+    this.privacyPolicy = checkbox.checked;
+  }
 }
