0912fb9e26c2b7bf9025f4136460491cea53c7e9
Henning Otte <HenningOtte91@gmail.com>
2026-01-05 20:13:44 +0100

    feat(about-me): implement texts from English JSON

--- src/app/main-content/about-me/about-me.ts
+++ src/app/main-content/about-me/about-me.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import en from '../../assets/common.en.json';
+import { ICONS } from '../../config/icons.config';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
-export class AboutMe {}
+export class AboutMe {
+  aboutMe = en.ABOUT_ME;
+  icons = ICONS.ABOUT_ME;
+
+  ngOnInit(): void {
+    console.log(this.aboutMe.POINTS[0]);
+  }
+}
