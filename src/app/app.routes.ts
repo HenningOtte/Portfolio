cdadff6c4c68eec2daeeb0dc75b8fd92882a9768
Henning Otte <HenningOtte91@gmail.com>
2026-01-11 00:48:30 +0100

    feat(imprint): add imprint component with content, styling and responsive layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import {MainContent} from './main-content/main-content'
+import { MainContent } from './main-content/main-content';
+import { Imprint } from './imprint/imprint';
 
 export const routes: Routes = [
-    { path: '', component: MainContent },
+  { path: '', component: MainContent },
+  { path: 'imprint', component: Imprint },
 ];
