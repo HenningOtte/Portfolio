f942dc4f6c24ec7308c78d8bc52cf40e5dbff8c5
Henning Otte <HenningOtte91@gmail.com>
2026-01-20 16:54:36 +0100

    feat: integrate language service into hero component

--- src/app/main-content/hero/hero.ts
+++ src/app/main-content/hero/hero.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { LanguageService } from '../../services/language-service';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {
+export class Hero implements OnInit {
   icons = ['git_icon.svg', 'linkedin_icon.svg', 'email_icon.svg'];
+  languageService = inject(LanguageService);
+  hero: any;
+
+  ngOnInit(): void {
+    this.languageService.lang.subscribe((lang) => {
+      this.hero = lang == 'en' ? this.languageService.en.HERO : this.languageService.de.HERO;
+    });
+  }
 }
