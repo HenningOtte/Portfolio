c49125110486972587d0f5dc245135aaf4de068e
Henning Otte <HenningOtte91@gmail.com>
2026-01-28 16:23:22 +0100

    changed body height to 100dvh

--- src/app/config/projects.config.ts
+++ src/app/config/projects.config.ts
@@ -1,14 +1,14 @@
 export const PROJECT_LINKS = [
   {
     live: 'https://henningotte.github.io/6-join/',
-    github: 'https://github.com/HenningOtte/6-join',
+    github: 'https://join-henning.de/',
   },
   {
     live: 'https://henningotte.github.io/1bit-horror-game/',
-    github: 'https://github.com/HenningOtte/1bit-horror-game',
+    github: 'https://1bithorror.de/',
   },
   {
     live: 'https://henningotte.github.io/Pokedex/',
-    github: 'https://github.com/HenningOtte/Pokedex',
+    github: 'https://dex-henning.de/',
   },
 ];
